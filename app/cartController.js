b82f4b9c61cb5aec19bae67b1ca5136d1cb990e9
Maury Viana <mauryrv@gmail.com>
2017-07-27 22:01:40 -0300

    added some last changes

--- app/cartController.js
+++ app/cartController.js
@@ -22,12 +22,9 @@ function list()
 	
 		var cart = JSON.parse(tbCarrinho[i]);
         //montar table de pedidos
-        console.log(cart);
 		var product = getProductInfo(cart.idProduct);
 		var newRow = $("<tr>");
     	var cols = "";
-        console.log(product.image);
-        console.log(product.nome);
 		cols += "<td><input type=\"image\" src=\""+product.image+"\" width=\"50px\"> "+product.nome+"</td>";
 		cols += "<td> <link class=\"glyphicon glyphicon-minus\" onclick=\"subtractItemQty("+cart.idProduct+","+i+","+cart.Qty+")\"/><input type=\"text\" value=\""+cart.Qty+"\" size=\"1px\" disabled/><link class=\"glyphicon glyphicon-plus\" onclick=\"addItemQty("+cart.idProduct+","+i+","+cart.Qty+")\"/></td>";
 		cols += "<td>"+product.valor+"</td>";
@@ -60,6 +57,9 @@ function calcFreight()
 {
     freight = 10 //peggar com api dos correios
 
+    
+   //fnConsomeWs()
+    //calculateShip();
     document.getElementById("freightValue").innerHTML ="R$ "+freight.toFixed(2);
     total = total + freight;
     document.getElementById("totalValue").innerHTML ="R$ "+total.toFixed(2);
@@ -109,6 +109,71 @@ window.location.assign("cart.html")
 
 }
 
+function calculateShip () {
+        var url1 = "http://ws.correios.com.br/calculador/CalcPrecoPrazo.aspx?"
+        + "nCdEmpresa="
+        + "&sDsSenha="
+        + "&sCepOrigem=02724070"
+        + "&sCepDestino=71939360"
+        + "&nVlPeso=1"
+        + "&nCdFormato=1"
+        + "&nVlComprimento=20"
+        + "&nVlAltura=5"
+        + "&nVlLargura=15"
+        + "&sCdMaoPropria=s"
+        + "&nVlValorDeclarado=200"
+        + "&sCdAvisoRecebimento=s"
+        + "&nCdServico=41106"
+        + "&nVlDiametro=0"
+        + "&StrRetorno=xml";  
+        
+        console.log(url1)
 
 
+$.ajax({
+  url: url1,
+  
+  success: function (data) { 
+      console.log(data)
+    
+    
+    /* data contém o que foi retornado pelo webservice */; }
+ });
+       
+}
 
+
+function fnConsomeWs() {
+
+var sendjson = {   
+    "nCdEmpresa":"",
+    "sDsSenha":"",
+    "nCdServico":"41106",
+    "sCepOrigem":"02724070",
+    "sCepDestino":"71939360",
+    "nVlPeso":"1",
+    "nCdFormato":"1",
+    "nVlComprimento":"20",
+    "nVlAltura":"5",
+    "nVlLargura":"15",
+    "nVlDiametro":"0",
+    "sCdMaoPropria":"s",
+    "nVlValorDeclarado":"200",
+    "sCdAvisoRecebimento":"s"
+}
+
+
+$.ajax({
+  type: "POST",
+  //contentType: "application/json; charset=utf-8",
+  url: "http://ws.correios.com.br/calculador/CalcPrecoPrazo.asmx/CalcPrecoPrazo",
+  data: sendjson,
+  assync : false ,
+  crossdomain : false,
+  dataType: "jsonp",
+  success: function (data) {
+            console.log(data);
+        }
+});
+
+}
